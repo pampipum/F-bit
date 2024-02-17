850aaf25ba4fbe6301d5c4cd6e96b76f1b028d1c
Andre Lopes <andremarqueslps@hotmail.comm>
2024-02-17 07:31:42 +0530

    changed graph

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 <div class="container mx-auto flex flex-col md:flex-row justify-center items-center">
 	<div class="md:w-1/3 w-full">
 		<div class="space-y-5">
-			<h1 class="text-xl font-bold">Let's get cracking bones!</h1>
+			<h1 class="text-xl font-bold">Let's Retire on Bitcoin!</h1>
 			<div class="flex justify-center">
 				<InputForm />
 			</div>
