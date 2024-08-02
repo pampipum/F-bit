e9466c21aec9f5531f192ef27e55ea5eb3a4404e
Andre Lopes <andremarqueslps@hotmail.comm>
2024-08-02 14:44:14 +0100

    Improvements

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,10 @@
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte'],
-	preprocess: [vitePreprocess()],
-
-	kit: {
-		adapter: adapter({
-			// Use 'edge' runtime instead of 'edge: true'
-			runtime: 'edge',
-			// Keep the split option
-			split: true
-		}),
-		alias: {
-			'@/*': './path/to/lib/*'
-		}
-	}
+  kit: {
+    adapter: adapter()
+  }
 };
 
 export default config;
